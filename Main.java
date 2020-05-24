8ad70c1602e7253ad63331a814b12f9926137314
AnzhelikaHrebeniuk <48449969+AnzhelikaHrebeniuk@users.noreply.github.com>
2020-05-24 22:25:53 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Sort sort = new Sort("src/input.txt", "src/otput.txt");
+    }
+}
---
+++ Sort.java
@@ -0,0 +1,34 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Sort {
+    private List<String> list;
+
+    public Sort(String inputFile, String outputFile) {
+        list = new ArrayList<>();
+        try (Stream<String> stream = Files.lines(Paths.get(inputFile))) {
+            list = stream.collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        list.sort(Comparator.comparingInt(String::length));
+
+        try (PrintWriter writer = new PrintWriter(outputFile)) {
+            for (String line : list) {
+                writer.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        };
+
+    }
+}
